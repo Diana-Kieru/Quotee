c7afcf597238b7ee8191eaea49773d1f6f770fad
diana <dianakieru1@gmil.com>
2022-04-09 23:19:22 +0300

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { AuthorComponent } from './quote-author/author.component';
+import { FormComponent } from './quote-form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    AuthorComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
