fe0a1b668257d55a9c65adc6e8f37f53fe5238b9
diana <dianakieru1@gmil.com>
2022-04-10 18:25:17 +0300

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
-import { AuthorComponent } from './quote-author/author.component';
-import { FormComponent } from './quote-form/form.component';
+// import { FormsModule } from '@angular/forms'
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent,
-    AuthorComponent,
-    FormComponent
+   
+  
   ],
   imports: [
     BrowserModule,
