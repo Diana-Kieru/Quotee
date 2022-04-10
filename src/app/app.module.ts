0a8a6b6ae3002f416b48e6f942a71e6fd3d92dbf
diana <dianakieru1@gmil.com>
2022-04-10 18:45:12 +0300

    added hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { HeroComponent } from './hero/hero.component';
 // import { FormsModule } from '@angular/forms'
 
 
@@ -12,6 +13,7 @@ import { FormComponent } from './form/form.component';
   declarations: [
     AppComponent,
     FormComponent,
+    HeroComponent,
    
   
   ],
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  newQuote = new Quotes('','','',new Date())
+  @Output() add = new EventEmitter()
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+submitQuote(){
+  this.add.emit(this.newQuote)
+  this.newQuote = new Quotes('','','',new Date())
+}
 }
