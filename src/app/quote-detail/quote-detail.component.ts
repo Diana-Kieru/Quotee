c9a284a65549246aaca8f99561abc74d49a93681
diana <dianakieru1@gmil.com>
2022-04-10 21:26:23 +0300

    added quote-detail content

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quote-detail',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+  @Input() quote :Quotes;
+  @Output() delete = new EventEmitter()
+  vote = 0;
+  down =0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  addUps(){
+    this.vote = this.vote+ 1
+  }
+  addDowns(){
+    this.down = this.down +1
+  }
+  quoteDelete(){
+    this.delete.emit()
+  }
 }
